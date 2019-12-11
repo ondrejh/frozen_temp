f59cfb1ae5bdcdefd2a1cbf416f22fee4c5efab2
Ondrej Hejda <ondrejh.ck@email.cz>
2019-12-11 23:56:04 +0100

    how plotly works

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
     <meta name="description" content="Vizualizace chladu." />
     <meta name="keywords" content="zima, kureuska" />
     <link rel="stylesheet" type="text/css" href="style.css" media="screen, print" />
-    <script src="plotly-latest.min.js"></script>
+    <script src="plotly.min.js"></script>
 </head>
 
 <body class="home">
@@ -26,8 +26,13 @@
         <article class="main">
             <h2>Graf</h2>
             <div id='chart'></div>
+            <?php
+              echo "<script>".PHP_EOL;
+              echo "var trace1 = {x: [1, 2, 3, 4], y: [10, 15, 13, 17], type: 'scatter'}; var trace2 = {x: [1, 2, 3, 4], y: [16, 5, 11, 9], type: 'scatter'}; var data = [trace1, trace2]; Plotly.newPlot('chart',data);".PHP_EOL;
+              echo "</script>". PHP_EOL;
+            ?>
 
-            <?php // get data
+            <!--// get data
                 ini_set('display_errors', 1);
                 ini_set('display_startup_errors', 1);
                 error_reporting(E_ALL);
@@ -39,102 +44,43 @@
                 while($row = $data->fetchArray()) {
                     $entries[] = array(date("Y-m-d H:i", strtotime($row['stamp'])), $row['t1'], $row['t2']);
                 }
-                echo PHP_EOL;
+                echo "<script>". PHP_EOL;
+                echo "var t1 = {x [";
+                $first = True;
                 for($i = 0; $i < sizeof($entries); $i ++) {
-                    var_dump($entries[$i]);
-                    echo "<br>". PHP_EOL;
+                    if (! $first) echo ", ";
+                    else $first = False;
+                    echo "'". $entries[$i][0]. "'";
                 }
-            ?>
-            <!--
-                document.getElementById("last_reading_timestamp").innerText = <?php echo "'". date('j.n.Y', strtotime($maxts)). " v ". date('G:i', strtotime($maxts)). "'"; ?>;
-                document.getElementById("last_reading_t1").innerText = <?php echo "'". round($lastp[1], 3). "'"; ?>;
-                document.getElementById("last_reading_t2").innerText = <?php echo "'". round($lastp[2], 3). "'"; ?>;
-                document.getElementById("last_reading_ttot").innerText = <?php echo "'". round($lastp[0], 3). "'"; ?>;
-                var tHcol = '#B21B04';
-                var tLcol = '#009933';
-                var tCcol = '#3333ff';
-                <?php
-                // tarif high data lines
-                $cnt = 0;
-                foreach ($watH as $w) {
-                    echo "var t1". $cnt. " = {x: [";
-                    $cnt += 1;
-                    $first = true;
-                    foreach ($w as $e) {
-                        if ($first) $first = false;
-                        else echo ', ';
-                        echo "'". $e[0]. "'";
-                    }
-                    echo "], y: [";
-                    $first = true;
-                    foreach ($w as $e) {
-                        if ($first) $first = false;
-                        else echo ', ';
-                        echo $e[1];
-                    }
-                    echo "], name: 'Drahý [kW]', type: 'scatter', mode: 'lines', fill: 'tozeroy', line: {color: tHcol}};". PHP_EOL;
-                };
-                // tarif low data lines
-                $cnt = 0;
-                foreach ($watL as $w) {
-                    echo "var tL". $cnt. " = {x: [";
-                    $cnt += 1;
-                    $first = true;
-                    foreach ($w as $e) {
-                        if ($first) $first = false;
-                        else echo ', ';
-                        echo "'". $e[0]. "'";
-                    }
-                    echo "], y: [";
-                    $first = true;
-                    foreach ($w as $e) {
-                        if ($first) $first = false;
-                        else echo ', ';
-                        echo $e[1];
-                    }
-                    echo "], name: 'Levný [kW]', type: 'scatter', mode: 'lines', fill: 'tozeroy', line: {color: tLcol}};". PHP_EOL;
-                };
-                // changes data lines (dots)
-                echo "var tC = {x: [";
-                $first = true;
-                foreach ($changes as $c) {
-                    if ($first) $first = false;
-                    else echo ', ';
-                    echo "'". $c[0]. "'";
+                echo "], y [";
+                $first = True;
+                for($i = 0; $i < sizeof($entries); $i ++) {
+                    if (! $first) echo ", ";
+                    else $first = False;
+                    echo $entries[$i][1];
                 }
-                echo "], y: [";
-                $first = true;
-                foreach ($changes as $c) {
-                    if ($first) $first = false;
-                    else echo ', ';
-                    echo $c[1];
+                echo "], name: 't1', type: 'scatter', mode: 'lines', fill: 'tozeroy'";
+                echo "};".PHP_EOL;
+                echo "var t2 = {x [";
+                $first = True;
+                for($i = 0; $i < sizeof($entries); $i ++) {
+                    if (! $first) echo ", ";
+                    else $first = False;
+                    echo "'". $entries[$i][0]. "'";
                 }
-                echo "], name: 'Změny [kW]', type: 'scatter', mode: 'markers', line: {color: tCcol}};". PHP_EOL;
-                // list of all data lines to plot
-                echo "var data = [";
-                $cnt = 0;
-                foreach ($watH as $w) {
-                    if ($cnt) echo ", ";
-                    echo "tH". $cnt;
-                    $cnt += 1;
+                echo "], y [";
+                $first = True;
+                for($i = 0; $i < sizeof($entries); $i ++) {
+                    if (! $first) echo ", ";
+                    else $first = False;
+                    echo $entries[$i][2];
                 }
-                $cnt = 0;
-                foreach ($watL as $w) {
-                    echo ", tL". $cnt;
-                    $cnt += 1;
-                }
-                echo "];". PHP_EOL; // no changes dots
-                #echo ", tC];". PHP_EOL; // with chages dots
-                ?>
-                var layout = {
-                    yaxis: {
-                        title: 'Příkon [kW]'
-                    },
-                    margin: { t: 0},
-                    showlegend: false
-                };
-                Plotly.newPlot('chart', data, layout);
-            </script>-->
+                echo "], name: 't2', type: 'scatter', mode: 'lines', fill: 'tozeroy'";
+                echo "};". PHP_EOL;
+                echo "var data = [t1, t2];". PHP_EOL;
+                echo "var layout = {yaxis: {title: 'ahoj [stC]'}, margin: { t: 0}, showlegend: false };". PHP_EOL;
+                echo "Plotly.newPlot('chart', data, legend); </script>". PHP_EOL;
+            -->
         </article>
     </section>
 </body>
