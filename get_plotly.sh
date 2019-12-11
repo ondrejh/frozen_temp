e205c1aa78c00792ec07a76285f85c7d9b1a6268
Ondrej Hejda <ondrejh.ck@email.cz>
2019-12-11 23:55:41 +0100

    typo (maybe?)

--- get_plotly.sh
+++ get_plotly.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-wget -O /var/www/html/plotly.latest.min.js "https://cdn.plot.ly/plotly-latest.min.js?_ga=2.229893404.14094198.1576096423-1389777143.1576096423"
+wget -O /var/www/html/plotly.min.js "https://cdn.plot.ly/plotly-latest.min.js?_ga=2.229893404.14094198.1576096423-1389777143.1576096423"
