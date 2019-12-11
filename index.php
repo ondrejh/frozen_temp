cd1fdf0abc2e5f5ff2d1054f1bea76ebd564f6b4
Ondrej Hejda <ondrejh.ck@email.cz>
2019-12-12 00:23:49 +0100

    this is how it basicaly works

--- index.php
+++ index.php
@@ -27,16 +27,6 @@
             <h2>Graf</h2>
             <div id='chart'></div>
             <?php
-              echo "<script>".PHP_EOL;
-              echo "var trace1 = {x: [1, 2, 3, 4], y: [10, 15, 13, 17], type: 'scatter'}; var trace2 = {x: [1, 2, 3, 4], y: [16, 5, 11, 9], type: 'scatter'}; var data = [trace1, trace2]; Plotly.newPlot('chart',data);".PHP_EOL;
-              echo "</script>". PHP_EOL;
-            ?>
-
-            <!--// get data
-                ini_set('display_errors', 1);
-                ini_set('display_startup_errors', 1);
-                error_reporting(E_ALL);
-
                 $db = new SQLite3("data.sql");
                 $query = "SELECT * FROM 'readings' WHERE 'stamp' > datetime('now', '-3 days')";
                 $data = $db->query($query);
@@ -44,43 +34,43 @@
                 while($row = $data->fetchArray()) {
                     $entries[] = array(date("Y-m-d H:i", strtotime($row['stamp'])), $row['t1'], $row['t2']);
                 }
+                echo "vypis<br>". PHP_EOL;
+                for ($i=1; $i<sizeof($entries); $i++) echo var_dump($entries[$i]);
+                echo "konec vypisu<br>". PHP_EOL;
                 echo "<script>". PHP_EOL;
-                echo "var t1 = {x [";
+                echo "var t1 = {x: [";
                 $first = True;
-                for($i = 0; $i < sizeof($entries); $i ++) {
+                for($i=1; $i<sizeof($entries); $i++) {
                     if (! $first) echo ", ";
                     else $first = False;
                     echo "'". $entries[$i][0]. "'";
                 }
-                echo "], y [";
+                echo "], y: [";
                 $first = True;
-                for($i = 0; $i < sizeof($entries); $i ++) {
+                for($i=1; $i<sizeof($entries); $i++) {
                     if (! $first) echo ", ";
                     else $first = False;
                     echo $entries[$i][1];
                 }
-                echo "], name: 't1', type: 'scatter', mode: 'lines', fill: 'tozeroy'";
-                echo "};".PHP_EOL;
-                echo "var t2 = {x [";
+                echo "], type: 'scatter'};".PHP_EOL;
+                echo "var t2 = {x: [";
                 $first = True;
-                for($i = 0; $i < sizeof($entries); $i ++) {
+                for($i=1; $i<sizeof($entries); $i++) {
                     if (! $first) echo ", ";
                     else $first = False;
                     echo "'". $entries[$i][0]. "'";
                 }
-                echo "], y [";
+                echo "], y: [";
                 $first = True;
-                for($i = 0; $i < sizeof($entries); $i ++) {
+                for($i=1; $i<sizeof($entries); $i++) {
                     if (! $first) echo ", ";
                     else $first = False;
                     echo $entries[$i][2];
                 }
-                echo "], name: 't2', type: 'scatter', mode: 'lines', fill: 'tozeroy'";
-                echo "};". PHP_EOL;
+                echo "], type: 'scatter'};". PHP_EOL;
                 echo "var data = [t1, t2];". PHP_EOL;
-                echo "var layout = {yaxis: {title: 'ahoj [stC]'}, margin: { t: 0}, showlegend: false };". PHP_EOL;
-                echo "Plotly.newPlot('chart', data, legend); </script>". PHP_EOL;
-            -->
+                echo "Plotly.newPlot('chart', data); </script>". PHP_EOL;
+            ?>
         </article>
     </section>
 </body>
