6078a9e45c530564afa61c38e3e685528d1d5dcb
Ondrej Hejda <ondrej.hejda@saviotechnics.com>
2019-12-20 12:43:52 +0100

    error count

--- display/display.ino
+++ display/display.ino
@@ -29,6 +29,8 @@ const String search_end = "<";
 
 const uint32_t updateTime_ms = 30000;
 
+#define MAX_ERRS 3
+
 ESP8266WiFiMulti WiFiMulti;
 
 // ntp client
@@ -67,13 +69,19 @@ String find_in_string(String str, const String *sb, int nsb, const String sa) {
 void loop() {
   static bool first = true;
 
+  static bool ntp_synchronized = false;
+  static bool t1_ready = false, t2_ready = false;
+  static int t1_errs = 0, t2_errs = 0;
+
   uint32_t now = millis();
-  static uint32_t tick = millis();
+  static uint32_t tick = now;
 
   // upgrade internet data
   if ((int32_t)(tick - now) <= 0) {
     tick = now + updateTime_ms;
     
+    String i1 = "", i2 = "";
+  
     // wait for WiFi connection
     if((WiFiMulti.run() == WL_CONNECTED)) {
       if (first) {
@@ -86,36 +94,48 @@ void loop() {
       HTTPClient http;
       http.begin(url); //HTTP
       int httpCode = http.GET();
-  
+
       // httpCode will be negative on error
       if(httpCode > 0) {
         // file found at server
         if(httpCode == HTTP_CODE_OK) {
           String payload = http.getString();
           //USE_SERIAL.println(payload);
-          String i1 = find_in_string(payload, search_t1, 3, search_end);
-          USE_SERIAL.print("T1: ");
-          USE_SERIAL.print(i1);
-          USE_SERIAL.print("°C");
-          String i2 = find_in_string(payload, search_t2, 3, search_end);
-          USE_SERIAL.print("  T2: ");
-          USE_SERIAL.print(i2);
-          USE_SERIAL.print("°C");
-          USE_SERIAL.println();
+          i1 = find_in_string(payload, search_t1, 3, search_end);
+          i2 = find_in_string(payload, search_t2, 3, search_end);
         }
       } else {
         USE_SERIAL.printf("http get failed (%s)\n", http.errorToString(httpCode).c_str());
       }
   
       http.end();
+      
     }
     else
       first = true;
-  
-    timeClient.update();
-    USE_SERIAL.print(timeClient.getFormattedTime());
-    USE_SERIAL.println();
-  }
 
-  
+    if (i1 != "") {t1_errs = 0; t1_ready = true;} else {if (t1_errs >= MAX_ERRS) t1_ready = false; else t1_errs ++;}
+    if (i2 != "") {t2_errs = 0; t2_ready = true;} else {if (t2_errs >= MAX_ERRS) t2_ready = false; else t2_errs ++;}
+
+    if ((i1 != "") && (t1_ready) && (i2 != "") && (t2_ready)) {
+      USE_SERIAL.print("T1: ");
+      USE_SERIAL.print(i1);
+      USE_SERIAL.print("°C"  );
+      USE_SERIAL.print("  T2: ");
+      USE_SERIAL.print(i2);
+      USE_SERIAL.print("°C");
+      USE_SERIAL.println();
+    }
+
+    timeClient.update();
+    if (ntp_synchronized) {
+      USE_SERIAL.print(timeClient.getFormattedTime());
+      USE_SERIAL.println();
+    }
+    else {
+      // stupid way how to find if its synchronized
+      if (timeClient.getEpochTime() > 0x1234)
+        ntp_synchronized = true;
+    }
+  }
 }
